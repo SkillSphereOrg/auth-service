01b2b08b0d60011daa2e4ca0da9bfe3cd7a6bcce
chithaprabhakaran46 <chithaprabhakaran46@gmail.com>
2025-07-17 01:19:59 +0530

    feat: add User and Role entities with JPA mappings for authentication and RBAC

---
+++ src/main/java/com/skillsphere/auth_service/model/Role.java
@@ -0,0 +1,19 @@
+package com.skillsphere.auth_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
