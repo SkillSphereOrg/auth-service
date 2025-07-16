8b647c041c8b0738562ce3ec23f48aafd6109391
chithaprabhakaran46 <chithaprabhakaran46@gmail.com>
2025-07-17 02:23:55 +0530

    fix: Jwt parser error

--- src/main/java/com/skillsphere/auth_service/security/JwtUtil.java
+++ src/main/java/com/skillsphere/auth_service/security/JwtUtil.java
@@ -35,7 +35,7 @@ public class JwtUtil {
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = Jwts.parserBuilder()
+        final Claims claims = Jwts.parser()
                 .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
